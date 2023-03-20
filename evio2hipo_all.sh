28c4f694c18077d599d8326145b625f7ecd3b639
bleaktwig <bruno.benkel@gmail.com>
2023-03-20 13:48:03 -0300

    Updated evio2hipo_all

--- evio2hipo_all.sh
+++ evio2hipo_all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Run evio2hipo on all evio files.
+VOL="/volatile/clas12/benkel/simul"
+EVIO="$VOL/gemc_out/evio"
+HIPO_IN="$VOL/gemc_out/hipo"
+
 counter=0
-for file in gemc_out/evio/*; do
-    evio2hipo -o "$PWD/gemc_out/hipo/torus-0.5_solenoid-0.745_999106_$counter.hipo" $file
+for file in $EVIO/*; do
+    evio2hipo -o "$HIPO_IN/simul_999106_$counter.hipo" $file
     ((counter++))
 done;
