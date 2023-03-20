fb24e3ee1ae12bfe7843a70fa61f6cff5970546d
bleaktwig <bruno.benkel@gmail.com>
2023-03-20 13:27:46 -0300

    Updated create_lund_batches

--- create_lund_batches.sh
+++ create_lund_batches.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+VOL="/volatile/clas12/benkel/simul"
+DAT="$VOL/lund/dat"
+BATCHES="$VOL/lund/batches"
+
 batch_no=0
 counter=0
-for file in lund/dat/*; do
-    batch_filename="lund/batches/batch_$batch_no.dat"
+for file in $DAT/*; do
+    if [ $counter -eq 0]; then
+        echo "working on batch $batch_no"
+    fi;
+    batch_filename="$BATCHES/batch_$batch_no.dat"
     if [ $counter -eq 0 ]; then
         touch $batch_filename
     fi;
