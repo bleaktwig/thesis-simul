e484d2da08f18bb7bd3cc77381832686614633a0
bleaktwig <bruno.benkel@gmail.com>
2023-03-21 16:51:04 -0300

    Whoops 2

--- sbatch_evio2hipo.sh
+++ sbatch_evio2hipo.sh
@@ -7,6 +7,6 @@ HIPO_IN="$VOL/gemc_out/hipo"
 
 counter=0
 for file in $EVIO/*; do
-    sbatch --mem=2048 $WORK/evio2hipo "$file" "$HIPO_IN/simul_999106_$counter.hipo"
+    sbatch --mem=2048 $WORK/evio2hipo.sh "$file" "$HIPO_IN/simul_999106_$counter.hipo"
     ((counter++))
 done;
