39f653d2a57d02df9c54e68aa0aa4cda5fc6f5e7
bleaktwig <bruno.benkel@gmail.com>
2023-04-28 15:29:30 -0400

    Fixed error in yaml

--- run_gemc.sh
+++ run_gemc.sh
@@ -2,4 +2,4 @@
 # Run GEMC on the given input lund file using the RG-F summer gcard.
 source vars.sh
 
-gemc $GCARD -USE_GUI=0 -INPUT_GEN_FILE="lund, $1" -OUTPUT="hipo, $2" -RANDOMIZE_LUND_VZ="-1.0*cm, 19.0*cm, reset"
+gemc $GCARD -USE_GUI=0 -INPUT_GEN_FILE="lund, $1" -OUTPUT="hipo, $2" -RANDOMIZE_LUND_VZ="-5.0*cm, 25.0*cm, reset"
